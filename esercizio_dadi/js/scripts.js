ea492d300e61777c408b68700d36f9bd27d90414
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2023-12-06 16:45:10 +0100

    (ex. dadi) - SETT random number function - SETT condition for compare values - ADD stamp in html - ADD style

--- esercizio_dadi/js/scripts.js
+++ esercizio_dadi/js/scripts.js
@@ -1,2 +1,29 @@
-// Test funzionamento
-console.log('Template');
+const startRoll = document.getElementById('start-roll');
+
+
+startRoll.addEventListener('click' , function() {
+    const player = document.getElementById('dado-player-1');
+    const cpu = document.getElementById('dado-player-2');
+    let numberPlayer1 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+    let numberPlayer2 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+    let winner = document.getElementById('winner');
+
+    player.innerHTML = numberPlayer1;
+    cpu.innerHTML = numberPlayer2;
+
+    if(numberPlayer1 == numberPlayer2){
+        winner.innerHTML = ('Pareggio');
+
+        
+    }else if (numberPlayer1 > numberPlayer2){
+        winner.innerHTML = ('Player 1 ha vinto !');
+
+
+    }else {
+        winner.innerHTML = ('Player 2 ha vinto !');
+
+
+    }
+
+}
+);
