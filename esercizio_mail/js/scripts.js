bd44ba273e03a90b4830beaf33fe898bbe789d65
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2023-12-06 16:02:29 +0100

    - ADD html stamp from js

--- esercizio_mail/js/scripts.js
+++ esercizio_mail/js/scripts.js
@@ -5,9 +5,19 @@ input.addEventListener('click' , function(){
 
     const userInput = document.getElementById('user-email').value;
     const validValues = ['alessio@gmail.com' , 'mauro@gmail.com' , 'marco@gmail.com'];
+    const container = document.querySelector('main > div');
 
-    if (userInput == validValues[0 , 1 , 2]){
-        alert("L'email è stata inserita correttamente");
+    if (userInput == validValues[0] || [1] || [3]){
+        container.innerHTML += `
+        <div class="card border-success m-auto mt-3" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title text-success"><i class="fa-solid fa-square-check"></i></h5>
+                <h6> Email inserita correttamente </h6>
+            </div>
+        </div>
+        
+        `
+
     }else{
         alert("Insersci una email valida")
     }
